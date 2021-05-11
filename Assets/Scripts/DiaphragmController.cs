40c2ce89c8f6bd4fedc7098ae58d8b0cd8bd0692
罗杨一飞 <19373479@buaa.edu.cn>
2021-05-11 17:56:51 +0800

    add camera and finish diaphragm

--- Assets/Scripts/DiaphragmController.cs
+++ Assets/Scripts/DiaphragmController.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DiaphragmController : MonoBehaviour
 {
-    private int diaphragmSize;//1: 2mm   2: 4mm     3: 8mm    0:null
-    private int waveLength;//1:365.0nm   2:404.7nm     3:435.8nm     4:546.1nm     5:577.0nm    0:null
+    public int diaphragmSize;//1: 2mm   2: 4mm     3: 8mm    0:null
+    public int waveLength;//1:365.0nm   2:404.7nm     3:435.8nm     4:546.1nm     5:577.0nm    0:null
+    public Text wave;
+    public Text size;
 
     public DiaphragmController(int diaphragmSize, int waveLength)
     {
@@ -13,8 +16,6 @@ public class DiaphragmController : MonoBehaviour
         this.waveLength = waveLength;
     }
 
-    public int DiaphragmSize { get => diaphragmSize; set => diaphragmSize = value; }
-    public int WaveLength { get => waveLength; set => waveLength = value; }
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +27,57 @@ public class DiaphragmController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(diaphragmSize == 1)
+        {
+            size.text = "当前孔径：2mm";
+        }
+        else if (diaphragmSize == 2)
+        {
+            size.text = "当前孔径：4mm";
+        }
+        else if (diaphragmSize == 3)
+        {
+            size.text = "当前孔径：8mm";
+        }
+        else
+        {
+            size.text = "当前孔径：未放置";
+        }
+
+        if(waveLength == 1)
+        {
+            wave.text = "当前光波长：365.0nm";
+        }
+        else if (waveLength == 2)
+        {
+            wave.text = "当前光波长：404.7nm";
+        }
+        else if (waveLength == 3)
+        {
+            wave.text = "当前光波长：435.8nm";
+        }
+        else if (waveLength == 4)
+        {
+            wave.text = "当前光波长：546.1nm";
+        }
+        else if (waveLength == 5)
+        {
+            wave.text = "当前光波长：577.0nm";
+        }
+        else
+        {
+            wave.text = "当前光波长：未放置";
+        }
+
+    }
+    public void changeSize()
+    {
+        if (diaphragmSize == 3) diaphragmSize = 0;
+        else diaphragmSize++;
+    }
+    public void changeWave()
+    {
+        if (waveLength == 5) waveLength = 0;
+        else waveLength++;
     }
 }
---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    float speed = 20f;
+    private Vector3 center = new Vector3(0f,0f,0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(transform.eulerAngles.y);
+        if (Input.GetKey(KeyCode.A))
+        {
+            if(transform.eulerAngles.y < 30f || transform.eulerAngles.y >180)
+            transform.RotateAround(center, Vector3.up, speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            if((transform.eulerAngles.y>-30f&& transform.eulerAngles.y<180f) || transform.eulerAngles.y > 330f)
+            transform.RotateAround(center, Vector3.up, -speed * Time.deltaTime);
+        }
+    }
+}
