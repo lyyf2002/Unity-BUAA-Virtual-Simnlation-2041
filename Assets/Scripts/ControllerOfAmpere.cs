e14e4e8e845e2626355d56d5390133b032d15197
罗杨一飞 <19373479@buaa.edu.cn>
2021-05-25 13:03:46 +0800

    finish most

--- Assets/Scripts/ControllerOfAmpere.cs
+++ Assets/Scripts/ControllerOfAmpere.cs
@@ -6,6 +6,8 @@ public class ControllerOfAmpere : MonoBehaviour
 {
     // Start is called before the first frame update
     public double ampere;
+    public double randomValue;
+    public bool isReset;
     public DiaphragmController diaphragmController;
     public GameObject diaphragm;
     public GameObject mercuryLamp;
@@ -14,58 +16,77 @@ public class ControllerOfAmpere : MonoBehaviour
     void Start()
     {
         this.ampere = 0;
+        this.isReset = false;
+        updateRandomValue();
     }
-    
+
+    public void reset()
+    {
+        isReset = true;
+    }
+
     // Update is called once per frame
     void Update()
     {
         this.ampere = getAmpere(diaphragmController, diaphragm, mercuryLamp, controllerOfVoltmeter);
     }
 
+    public void updateRandomValue()
+    {
+        System.Random rnd = new System.Random();
+        this.randomValue = rnd.Next(-1, 1) * 0.1;
+    }
+
     public double getAmpere(DiaphragmController diaphragmController, GameObject diaphragm, GameObject mercuryLamp, ControllerOfVoltmeter controllerOfVoltmeter)
     {
+        if (!isReset) return 0;
         int _diaphragmSize = diaphragmController.diaphragmSize;//1: 2mm   2: 4mm     3: 8mm    0:null
         int _waveLength = diaphragmController.waveLength;//1:365.0nm   2:404.7nm     3:435.8nm     4:546.1nm     5:577.0nm    0:null
-        
+
+        if (_diaphragmSize == 0 || _waveLength == 0) return 0;
+
         // double _length = diaphragm.transform.position.x - mercuryLamp.transform.position.x;
-        double _volt = controllerOfVoltmeter.volt;
+        double _volt = (double)controllerOfVoltmeter.volt / 10f;
         int _int_volt = (int)_volt;
 
         double ampere_1, ampere_2, ampere;
+
+
         if (_waveLength == 1)
         {
-            double[] amperes = { 2.3, 10.4, 17.7, 23.5, 28.0, 31.2, 33.5, 35.2, 36.5, 37.7, 42.2 };
+            double[] amperes = { 2.3, 10.4, 17.7, 23.5, 28.0, 31.2, 33.5, 35.2, 36.5, 37.7, 42.2, 42.2 };
             ampere_1 = _int_volt == -1 ? 0.7 : amperes[_int_volt / 5];
             ampere_2 = _int_volt == -1 ? 0.7 : amperes[_int_volt / 5 + 1];
         }
         else if (_waveLength == 2)
         {
-            double[] amperes = { 1.0, 4.8, 8.1, 10.6, 12.5, 13.8, 15.7, 16.8, 15.7, 15.9, 15.7 };
+            double[] amperes = { 1.0, 4.8, 8.1, 10.6, 12.5, 13.8, 15.7, 16.8, 15.7, 15.9, 15.7, 15.7 };
             ampere_1 = _int_volt == -1 ? 0.2 : amperes[_int_volt / 5];
             ampere_2 = _int_volt == -1 ? 0.2 : amperes[_int_volt / 5 + 1];
         }
         else if (_waveLength == 3)
         {
-            double[] amperes = { 1.8, 8.5, 14.4, 19.7, 21.7, 23.5, 26.2, 27.6, 28.5, 27.0, 27.7 };
+            double[] amperes = { 1.8, 8.5, 14.4, 19.7, 21.7, 23.5, 26.2, 27.6, 28.5, 27.0, 27.7, 27.7 };
             ampere_1 = _int_volt == -1 ? 0.2 : amperes[_int_volt / 5];
             ampere_2 = _int_volt == -1 ? 0.2 : amperes[_int_volt / 5 + 1];
         }
         else if (_waveLength == 4)
         {
-            double[] amperes = { 0.3, 1.5, 1.9, 2.2, 2.7, 2.9, 2.8, 2.9, 2.9, 3.0, 3.1 };
+            double[] amperes = { 0.3, 1.5, 1.9, 2.2, 2.7, 2.9, 2.8, 2.9, 2.9, 3.0, 3.1, 3.1 };
             ampere_1 = _int_volt == -1 ? 0.1 : amperes[_int_volt / 5];
             ampere_2 = _int_volt == -1 ? 0.1 : amperes[_int_volt / 5 + 1];
         }
         else
         {
-            double[] amperes = { 0.2, 1.0, 1.4, 1.6, 1.8, 1.9, 2.0, 2.0, 2.1, 2.1, 2.1 };
+            double[] amperes = { 0.2, 1.0, 1.4, 1.6, 1.8, 1.9, 2.0, 2.0, 2.1, 2.1, 2.1, 2.1 };
             ampere_1 = _int_volt == -1 ? 0.1 : amperes[_int_volt / 5];
             ampere_2 = _int_volt == -1 ? 0.1 : amperes[_int_volt / 5 + 1];
         }
+        //Debug.Log((double)(_volt - ((int)_int_volt / 5 * 5)) / 5f * (ampere_2 - ampere_1));
+        Debug.Log(ampere_2);
+        Debug.Log(ampere_1);
+        ampere = (double)(_volt - ((int)_int_volt/5*5)) / 5f * (ampere_2 - ampere_1) + ampere_1;
 
-        ampere = (_volt - _int_volt) / 5 * (ampere_2 - ampere_1) + ampere_1;
-
-        System.Random rnd = new System.Random();
-        return ampere + rnd.Next(-1, 1) * 0.1;
+        return ampere + this.randomValue;
     }
 }
--- Assets/Scripts/ControllerOfVoltmeter.cs
+++ Assets/Scripts/ControllerOfVoltmeter.cs
@@ -1,29 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControllerOfVoltmeter : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float volt;
-    public float mode;
+    public int volt;
+    public int mode;
+    public int count;
+    public Text text_volt, test_currency;
+    public ControllerOfAmpere controllerOfAmpere;
     void Start()
     {
+        count = 1;
         volt = 0;
-        mode = 5;
+        mode = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int temp = (int)(volt);
+        if(temp >= 100)
+            text_volt.text = (temp/10).ToString() + "." + (temp%10).ToString();
+        else if(0 <= temp && temp < 100)
+            text_volt.text = "0" + (temp / 10).ToString() + "." + (temp % 10).ToString();
+        else if(temp > -10)
+            text_volt.text = "-0" + (temp / 10).ToString() + "." + (-temp % 10).ToString();
+        else
+            text_volt.text ="-01.0";
+        test_currency.text = "";
+        temp = (int)(controllerOfAmpere.ampere * 10f);
+      
+            test_currency.text = (temp / 1000).ToString()  + ((temp / 100) % 10).ToString() + ((temp / 10) % 10).ToString() + "." + (temp %10).ToString();
+           
+    }
+    public void setZero()
+    {
+        controllerOfAmpere.reset();
+        UnityEditor.EditorUtility.DisplayDialog("提示", "调零完成", "开始实验");
     }
     public void addVolt()
     {
-        volt += mode;
+        if (!controllerOfAmpere.isReset)
+        {
+            UnityEditor.EditorUtility.DisplayDialog("提示", "请先进行调零", "确认");
+        }
+            if (volt + mode <= 500)
+        {
+            volt += mode;
+        }
+        controllerOfAmpere.updateRandomValue();
     }
     public void subVolt()
     {
-        volt -= mode;
+        if (!controllerOfAmpere.isReset)
+        {
+            UnityEditor.EditorUtility.DisplayDialog("提示", "请先进行调零", "确认");
+        }
+        if (volt - mode >= -10)
+        {
+            volt -= mode;
+        }
+        controllerOfAmpere.updateRandomValue();
+    }
+    public void modeUp()
+    {
+        if (!controllerOfAmpere.isReset)
+        {
+            UnityEditor.EditorUtility.DisplayDialog("提示", "请先进行调零", "确认");
+        }
+        if (count < 3)
+        {
+            count++;
+            mode *= 10;
+        }
+    }
+    public void modeDown()
+    {
+        if (!controllerOfAmpere.isReset)
+        {
+            UnityEditor.EditorUtility.DisplayDialog("提示", "请先进行调零", "确认");
+        }
+        if (count > 1)
+        {
+            count--;
+            mode /= 10;
+        }
+        //Debug.Log(mode);
     }
 }
